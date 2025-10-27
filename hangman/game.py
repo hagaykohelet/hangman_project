cfcb5ba0e790441846bcabd0e52dcab5d296d8f4
hagay kohelet <hagay2506@gmail.com>
2025-10-27 10:26:05 +0200

    init state, validate_gusses,aplly_guess funcs

---
+++ hangman/game.py
@@ -0,0 +1,28 @@
+from words import choose_secret_word
+
+def init_state(secret: str, max_tries: int) -> dict:
+    state_game = {"secret" : secret,
+                 "display": list[str],
+                 "guessed": set[str],
+                 "wrong_guesses": int,
+                 "max_tries": max_tries}
+    return state_game
+
+
+def validate_guesses(ch:str, guessed: set[str]) -> tuple[bool, str]:
+    if len(ch) == 1:
+        if ch in guessed:
+            return False, f"{ch} alraedy exist"
+        else:
+            return True, f"You hit the right letter {ch}"
+    else:
+        raise "you need enter only one word"
+
+def apply_guess(state:dict, ch:str) -> bool:
+    if ch in state["secret"]:
+        return False
+    else:
+        return True
+
+
+
